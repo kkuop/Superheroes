39e619d21f0580b55ce9a57ab863735f147c8224
kkuop <60794860+kkuop@users.noreply.github.com>
2020-03-16 15:19:42 -0500

    added 3 views for the SuperheroesController

--- SuperheroesWebApp/Data/ApplicationDbContext.cs
+++ SuperheroesWebApp/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperheroesWebApp.Models;
 
 namespace SuperheroesWebApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Superhero> Superhero { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
