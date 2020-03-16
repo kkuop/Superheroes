9b713ef10d3d492277df0351457ba278cce1023b
kkuop <60794860+kkuop@users.noreply.github.com>
2020-03-16 15:15:47 -0500

    added Superhero class and setup properties

---
+++ SuperheroesWebApp/Models/Superhero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperheroesWebApp.Models
+{
+    public class Superhero
+    {
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+        public Superhero()
+        {
+
+        }
+    }
+}
